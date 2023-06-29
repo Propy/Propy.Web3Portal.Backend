82b05db70d17eb98c799fcd7c313b0fc03df183d
Jay Welsh <thealchemicalopus@gmail.com>
2023-06-29 17:29:10 +0200

    feat(AssetController): manual metadata refreshing

--- src/controllers/AssetController.ts
+++ src/controllers/AssetController.ts
@@ -1,4 +1,5 @@
 import e, { Request, Response } from 'express';
+import { validationResult } from "express-validator";
 import { utils } from "ethers";
 
 import {
@@ -12,6 +13,10 @@ import Controller from './Controller';
 import {
   AssetOutputTransformer,
 } from '../database/transformers';
+
+import {
+	syncTokenMetadata
+} from '../tasks/sync-token-metadata';
 
 BigNumber.config({ EXPONENTIAL_AT: [-1e+9, 1e+9] });
 
@@ -39,6 +44,34 @@ class AssetController extends Controller {
     let assetInfo = await AssetRepository.getAssetByAddressAndNetworkAndTokenId(assetAddress, network, tokenId, AssetOutputTransformer);
 
     this.sendResponse(res, assetInfo ? assetInfo : {});
+
+  }
+
+  async refreshAssetMetadata(req: Request, res: Response) {
+
+    const errors = await validationResult(req);
+    if (!errors.isEmpty()) {
+      return this.sendResponse(res, {errors: errors.array()}, "Validation error", 422);
+    }
+
+    const {
+      network = "",
+      asset_address = "",
+      token_id = "",
+    } = req.body;
+
+    let assetInfo = await AssetRepository.getAssetByAddressAndNetworkAndTokenId(asset_address, network, token_id);
+
+    if((assetInfo?.balance?.length > 0) && (["ERC-721"].indexOf(assetInfo?.standard) > -1)) {
+      try {
+        await syncTokenMetadata(assetInfo?.balance, assetInfo.standard);
+        return this.sendResponse(res, { message: "Token metadata successfully refreshed" });
+      } catch (e) {
+        return this.sendError(res, 'Error refreshing token metadata, please contact support if problem persists.', 500);
+      }
+    } else {
+      return this.sendError(res, 'Token record not found, please contact support if problem persists.', 500);
+    }
 
   }
 }
--- src/routes/asset.routes.ts
+++ src/routes/asset.routes.ts
@@ -1,9 +1,21 @@
 'use strict';
 
+import { body } from 'express-validator';
+
+import {
+  isETHAddress,
+} from "../web3/utils";
+
 import Router from "./Router";
 
 Router.get('/asset/:network/:assetAddress', [], 'AssetController@getAssetInfo');
 
 Router.get('/asset/:network/:assetAddress/:tokenId', [], 'AssetController@getAssetInfoWithTokenId');
+
+Router.post('/asset/refresh-metadata', [
+  body('network').notEmpty(),
+  body('asset_address').notEmpty().custom(isETHAddress),
+  body('token_id').notEmpty(),
+], 'AssetController@refreshAssetMetadata');
 
 module.exports = Router.export();
