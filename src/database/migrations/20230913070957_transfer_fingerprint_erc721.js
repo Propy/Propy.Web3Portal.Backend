e563e1aaf43733c320b20a0a349fa32b30afa6d1
Jay Welsh <thealchemicalopus@gmail.com>
2023-09-13 11:21:45 +0200

    feat(BalanceRepository): indexed nft fingerprints

---
+++ src/database/migrations/20230913070957_transfer_fingerprint_erc721.js
@@ -0,0 +1,26 @@
+const {
+  ERC721_TRANSFER_EVENT_TABLE,
+} = require("../tables");
+
+exports.up = (knex) => knex.schema.alterTable(ERC721_TRANSFER_EVENT_TABLE, table => {
+  table.string("event_fingerprint");
+}).then(async () => {
+  await knex(ERC721_TRANSFER_EVENT_TABLE)
+  .select('id')
+  .select('network_name')
+  .select('block_number')
+  .select('transaction_index')
+  .select('log_index')
+  .then(async (events) => {
+    for(let event of events) {
+      let eventFingerprint = `${event.network_name}-${event.block_number}-${event.transaction_index}-${event.log_index}`;
+      await knex(ERC721_TRANSFER_EVENT_TABLE).update({"event_fingerprint": eventFingerprint}).where('id', event.id);
+    }
+  });
+}).then(() => knex.schema.alterTable(ERC721_TRANSFER_EVENT_TABLE, table => {
+  table.string("event_fingerprint").index().unique().notNullable().alter();
+}))
+
+exports.down = (knex) => knex.schema.alterTable(ERC721_TRANSFER_EVENT_TABLE, table => {
+  table.dropColumn("event_fingerprint");
+});
