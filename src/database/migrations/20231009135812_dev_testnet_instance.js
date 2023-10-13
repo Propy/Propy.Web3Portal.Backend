8b40c0f7d9f3b808437aca28b427784b7da84e44
Jay Welsh <thealchemicalopus@gmail.com>
2023-10-13 16:08:01 +0200

    feat(migations): fix dev-specific migration

--- src/database/migrations/20231009135812_dev_testnet_instance.js
+++ src/database/migrations/20231009135812_dev_testnet_instance.js
@@ -8,23 +8,25 @@ dotenv.config();
 
 const APP_ENV = process.env.APP_ENV || "prod";
 
-exports.up = (knex) => knex(ASSET_TABLE).insert(
-  [
-    ((APP_ENV === 'dev' || APP_ENV === 'stage') &&
-      {
-        address: "0x73C3a1437B0307732Eb086cb2032552eBea15444",
-        network_name: "goerli",
-        symbol: "pDHV2",
-        is_base_asset: false,
-        deployment_block: "9561320",
-        standard: "ERC-721",
-        decimals: 0,
-        name: "PropyDeedHashedV2",
-        slug: "propy-deed-certificates-dev-testnet",
-        collection_name: "Propy Certificates Dev (Testnet)",
-      }
-    ),
-  ].filter(Boolean)
-);
+exports.up = (knex) => {
+  if((APP_ENV === 'dev' || APP_ENV === 'stage')) {
+    knex(ASSET_TABLE).insert(
+      [
+          {
+            address: "0x73C3a1437B0307732Eb086cb2032552eBea15444",
+            network_name: "goerli",
+            symbol: "pDHV2",
+            is_base_asset: false,
+            deployment_block: "9561320",
+            standard: "ERC-721",
+            decimals: 0,
+            name: "PropyDeedHashedV2",
+            slug: "propy-deed-certificates-dev-testnet",
+            collection_name: "Propy Certificates Dev (Testnet)",
+          }
+      ]
+    )
+  }
+};
 
 exports.down = knex => knex(ASSET_TABLE).where("address", "0x73C3a1437B0307732Eb086cb2032552eBea15444").delete();
