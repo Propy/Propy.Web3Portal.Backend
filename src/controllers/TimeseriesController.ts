fc08633a6710f4bfa231a768a679176a35a34029
Jay Welsh <thealchemicalopus@gmail.com>
2024-03-26 14:24:08 +0200

    feat(TimeseriesController): use generic cache

--- src/controllers/TimeseriesController.ts
+++ src/controllers/TimeseriesController.ts
@@ -7,19 +7,18 @@ import Controller from './Controller';
 
 import {
   TokenTransferEventERC721Repository,
+  GenericCacheRepository,
 } from '../database/repositories';
+
+import {
+  GENERIC_CACHE_KEYS,
+  GENERIC_CACHE_AGES,
+} from '../constants';
 
 BigNumber.config({ EXPONENTIAL_AT: [-1e+9, 1e+9] });
 
-class TimseriesController extends Controller {
-  async getMintedPerDay(req: Request, res: Response) {
-
-    const {
-      network,
-      contractAddress,
-    } = req.params;
-
-    const result = await TokenTransferEventERC721Repository.query()
+const generatePropykeysDailyMintTimeseries = async (network: string, contractAddress: string) => {
+  const result = await TokenTransferEventERC721Repository.query()
     .select(
       raw("DATE_TRUNC('day', to_timestamp(CAST(evm_transaction.block_timestamp AS numeric))) AS utc_day"),
       raw('COUNT(*) AS record_count')
@@ -30,6 +29,45 @@ class TimseriesController extends Controller {
     .where('erc721_transfer_event.network_name', network)
     .groupBy('utc_day')
     .orderBy('utc_day');
+
+    return result
+}
+class TimseriesController extends Controller {
+  async getMintedPerDay(req: Request, res: Response) {
+
+    const {
+      network = "",
+      contractAddress = "",
+    } = req.params;
+
+    // Check if we have a valid cached result
+    let cachedData = await GenericCacheRepository.findByColumn("key", GENERIC_CACHE_KEYS.PROPYKEYS_DAILY_MINT_COUNTS(network, contractAddress));
+    let result;
+
+    let currentTimeUnix = Math.floor(new Date().getTime() / 1000);
+    if(cachedData?.update_timestamp) {
+      let shouldUpdate = (currentTimeUnix - Number(cachedData?.update_timestamp)) > cachedData?.max_seconds_age;
+      if(shouldUpdate) {
+        result = await generatePropykeysDailyMintTimeseries(network, contractAddress);
+        this.sendResponse(res, result ? result : []);
+        await GenericCacheRepository.update({json: JSON.stringify(result), update_timestamp: currentTimeUnix, max_seconds_age: GENERIC_CACHE_AGES.PROPYKEYS_DAILY_MINT_COUNTS}, cachedData.id);
+        return;
+      } else {
+        result = cachedData.json;
+      }
+    } else {
+      result = await generatePropykeysDailyMintTimeseries(network, contractAddress);
+      try {
+        await GenericCacheRepository.create({
+          key: GENERIC_CACHE_KEYS.PROPYKEYS_DAILY_MINT_COUNTS(network, contractAddress),
+          update_timestamp: currentTimeUnix,
+          json: JSON.stringify(result),
+          max_seconds_age: GENERIC_CACHE_AGES.PROPYKEYS_DAILY_MINT_COUNTS
+        })
+      } catch(e) {
+        console.log("Error creating generic cache for coordinates");
+      }
+    }
 
     return this.sendResponse(res, result ? result : []);
 
